fef4562bc6cedfbfe7fe51ea76d555b1912aa671
Mir Billadin <mir.billadin4@gmail.com>
2023-07-26 06:56:22 +0600

    slider + responsiveness added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        daisyui: {
+          themes: ["light", "dark"],
+        }
+      },
     },
     plugins: [],
   }
